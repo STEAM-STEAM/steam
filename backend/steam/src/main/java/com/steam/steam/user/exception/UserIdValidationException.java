15313cc12e18dedb35059ed4ba81dd98f6147831
yyeennyy <69946286+yyeennyy@users.noreply.github.com>
2023-11-15 01:24:16 +0900

    fix: ID policy (#15)

---
+++ backend/steam/src/main/java/com/steam/steam/user/exception/UserIdValidationException.java
@@ -0,0 +1,7 @@
+package com.steam.steam.user.exception;
+
+public class UserIdValidationException extends Exception {
+    public UserIdValidationException(String message) {
+        super(message);
+    }
+}
