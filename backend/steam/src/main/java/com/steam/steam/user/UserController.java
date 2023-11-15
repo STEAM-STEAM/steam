1158c8c8a57dc4c9fa6d9b2ebedf6a1a48933c6e
skj4244 <skj4244@naver.com>
2023-11-15 21:51:59 +0900

    chore: connect Front to Back

--- backend/steam/src/main/java/com/steam/steam/user/UserController.java
+++ backend/steam/src/main/java/com/steam/steam/user/UserController.java
@@ -9,13 +9,11 @@ import com.steam.steam.user.exception.UserIdNotExistsException;
 import com.steam.steam.user.exception.UserIdValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:3000")
 public class UserController {
     private final UserService userService;
 
