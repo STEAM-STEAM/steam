02bc7b9ac9bec9b1912d3de293b50e95dccaec8b
rlcz1 <rbf0311@gmail.com>
2023-11-18 02:56:05 +0900

    feat: Login Process

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 import styled from "@emotion/styled";
 import Select from 'react-select'
 import { Link } from "react-router-dom";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faUser } from "@fortawesome/free-regular-svg-icons";
+import axios from 'axios'
 
-const JoinFrm = styled.div`
-    width: 100%;
+const LoginFrm = styled.div`
+    width: 500px;
+    margin-top: 50px;
 
     & > p {
         font-size: 22px;
@@ -23,22 +23,22 @@ const Item = styled.div`
     border-bottom: 1px solid #ddd;
     float: left;
 
-    &:last-of-type {
-        border-bottom: 1px solid #111
-    }
-
+    &:last-of-type {border-bottom: 1px solid #111}
     & > p {
         float: left;
-        width: 150px;
+        width: 100px;
         line-height: 35px;
     }
     & > div {
         float: left;
-        width: calc(100% - 150px);
+        width: calc(100% - 100px);
     }
     & > div > input {
+        border: solid 1px #ddd;
+        font-size: 14px;
         width: 100%;
         height: 35px;
+        border-radius: 5px;
     }
 `;
 
@@ -53,26 +53,69 @@ const Btn = styled.button`
 
 const Login = () => {
 
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+
+    const login = () => {
+        const data = {
+            id: id,
+            pw: pw
+        };
+        axios.post('http://localhost:8080/user/login', data).then(loginSuccess).catch(err => {
+            alert("아이디 또는 비밀번호를 다시 확인해주세요.");
+        });
+    }
+
+    const loginSuccess = (res) => {
+        const message = res.data.message;
+        if(message === "success") {
+            alert("로그인 성공");
+            window.location.href = "/";
+            userInfo();
+        } else if (message === "id_error") {
+            alert("아이디를 다시 확인해주세요.");
+        } else if (message === "pw_error") {
+            alert("비밀번호를 다시 확인해주세요.");
+        }
+    }
+
+    // 로그인 성공시 유저 정보 요청
+    const userInfo = () => {
+        axios.get(`http://localhost:8080/user/info/${id}`).then(res => {
+            const user = JSON.parse(res.data);
+            sessionStorage.setItem("user", user);
+        });
+    }
+
     return (
-        <div style={{width: 800, float: "left", left: "50%", transform: "translateX(-50%)"}}>
-            <JoinFrm>
+        <div style={{width: "100%", float: "left"}}>
+            <LoginFrm>
                 <p>로그인</p>
                 <Item>
                     <p>아이디</p>
                     <div>
-                        <input placeholder="아이디를 입력해주세요." />
+                        <input 
+                            placeholder="아이디를 입력해주세요." 
+                            onChange={(e) => setId(e.target.value)} 
+                            required 
+                        />
                     </div>
                 </Item>
                 <Item>
                     <p>비밀번호</p>
                     <div>
-                        <input placeholder="비밀번호를 입력해주세요." />
+                        <input
+                            type="password"
+                            placeholder="비밀번호를 입력해주세요."
+                            onChange={(e) => setPw(e.target.value)}
+                            required
+                        />
                     </div>
                 </Item>
                 <Item>
-                    <Btn>로그인</Btn>
+                    <Btn onClick={() => login}>로그인</Btn>
                 </Item>
-            </JoinFrm>
+            </LoginFrm>
         </div>
     );
 }
