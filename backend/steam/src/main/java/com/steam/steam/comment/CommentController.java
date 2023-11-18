862f2443d89caeec1edc74a491e85802db1efb1c
yyeennyy <69946286+yyeennyy@users.noreply.github.com>
2023-11-19 03:13:53 +0900

    fix: Profile (#67)
    * fix: Profile config
    
    * fix: Cors

--- backend/steam/src/main/java/com/steam/steam/comment/CommentController.java
+++ backend/steam/src/main/java/com/steam/steam/comment/CommentController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
     private final CommentService commentService;
 
