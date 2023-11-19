e21e553c41491b2b30c5043e7c6a7697e0f8a3d8
shxnzxxn <122579839+shxnzxxn@users.noreply.github.com>
2023-11-19 18:37:57 +0900

    fix: cascade error (#78)
    ;

--- backend/steam/src/main/java/com/steam/steam/user/Keyword.java
+++ backend/steam/src/main/java/com/steam/steam/user/Keyword.java
@@ -12,7 +12,7 @@ public class Keyword {
     @Column(name = "keyword_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
     private String keyword;
